a0e92ae455b18571e7309e2c4558c2b39ed025ee
Kerbidiah <alexjanninck@gmail.com>
2024-03-29 11:45:17 -0400

    temperature and disabling i3c

--- src/commands.rs
+++ src/commands.rs
@@ -1,16 +1,42 @@
 use super::*;
 
-// use i2c::ErrorKind;
-
 impl<I2C: I2c> Lsm6dsr<I2C> {
 
 	// page 50
-	const WHO_AM_I_VAL: u8 = 0b01101011;
-	// const WHO_AM_I_REGISTER: u8 = 0x0F;
+	const CORRECT_WHO_AM_I: u8 = 0b01101011;
+	/// checks to see if the chip reports the correct `WHO_AM_I` value
+	/// 
+	/// returns `Ok(true)` if the chip reports the correct value, `Ok(false)` if it
+	/// reports an incorrect value, and `Err()` if there was an error checking the value
 	pub fn is_valid(&mut self) -> Result<bool, I2C::Error> {
-		let mut dst = [0x00];
-		self.read_register(0x0F, &mut dst)?;
+		let id = self.read_byte(0x0f)?;
 
-		Ok(dst[0] == Self::WHO_AM_I_VAL)
+		Ok(id == Self::CORRECT_WHO_AM_I)
+	}
+
+	/// disable i3c (*not* i2c)
+	/// 
+	/// It is recomended to do this before doing any other configuration stuff
+	pub fn disable_i3c(&mut self) -> Result<(), I2C::Error> {
+
+		// get the other settings so we don't accidently change them
+		let mut settings = self.read_byte(0x18)?;
+
+		settings |= 0b00000010; // set the i3c disable bit to 1
+
+		self.write_byte(0x18, settings)
+	}
+
+	/// get temperature in degrees celcius
+	pub fn get_temp(&mut self) -> Result<f32, I2C::Error> {
+		let mut buf = [0x00; 2];
+
+		self.read_buf(0x20, &mut buf)?;
+
+		// convert using the correct byte order
+		let raw_temp = i16::from_le_bytes(buf) as f32;
+		let temperature = (raw_temp / 16.0) + 25.0;
+
+		Ok(temperature)
 	}
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,23 +2,23 @@ use embedded_hal::i2c::{self, I2c};
 
 mod commands;
 
-pub use commands::*;
+// pub use commands::*;
 
 #[derive(Debug)]
 pub struct Lsm6dsr<I2C> {
 	i2c: I2C,
-	g_scale: GScale,
-	filtering: bool // page 51
+	accel_scale: AccelScale,
+	accel_filtering: bool // page 51
 }
 
 #[repr(u8)]
 #[derive(Debug, Clone, Copy)]
 /// Accelerometer scale options (page 51)
-pub enum GScale {
+pub enum AccelScale {
 	Scale2 = 0,
 	Scale4 = 2,
-	Scale16 = 1,
 	Scale8 = 3,
+	Scale16 = 1,
 }
 
 impl<I2C: I2c> Lsm6dsr<I2C> {
@@ -28,29 +28,54 @@ impl<I2C: I2c> Lsm6dsr<I2C> {
 	pub fn new(i2c: I2C) -> Self {
 		Self {
 			i2c,
-			g_scale: GScale::Scale2,
-			filtering: false,
+			accel_scale: AccelScale::Scale2,
+			accel_filtering: false,
 		}
 	}
 
-	#[inline]
-	fn read(&mut self, read: &mut [u8]) -> Result<(), I2C::Error> {
-		self.i2c.read(Self::ADDRESS, read)
-	}
+	// #[inline]
+	// /// NOTE: you must handle registers yourself
+	// fn read(&mut self, read: &mut [u8]) -> Result<(), I2C::Error> {
+	// 	self.i2c.read(Self::ADDRESS, read)
+	// }
+
+	// #[inline]
+	// /// NOTE: you must handle registers yourself
+	// fn write(&mut self, write: &[u8]) -> Result<(), I2C::Error> {
+	// 	self.i2c.write(Self::ADDRESS, write)
+	// }
+
+	// #[inline]
+	// /// NOTE: you must handle registers yourself
+	// fn write_read(&mut self, write: &[u8], read: &mut [u8]) -> Result<(), I2C::Error> {
+	// 	self.i2c.write_read(Self::ADDRESS, write, read)
+	// }
 
 	#[inline]
-	fn write(&mut self, write: &[u8]) -> Result<(), I2C::Error> {
-		self.i2c.write(Self::ADDRESS, write)
-	}
-
-	#[inline]
-	fn write_read(&mut self, write: &[u8], read: &mut [u8]) -> Result<(), I2C::Error> {
-		self.i2c.write_read(Self::ADDRESS, write, read)
-	}
-
-	#[inline]
-	fn read_register(&mut self, register: u8, read: &mut [u8]) -> Result<(), I2C::Error> {
+	fn read_buf(&mut self, register: u8, read: &mut [u8]) -> Result<(), I2C::Error> {
 		self.i2c.write_read(Self::ADDRESS, &[register], read)
 	}
 
+	fn write_buf(&mut self, register: u8, write: &[u8]) -> Result<(), I2C::Error> {
+		// create a buf with the register as the first value
+		let mut buf = write.to_vec();
+		buf.insert(0, register);
+		
+		self.i2c.write(Self::ADDRESS, &buf)?;
+		
+		Ok(())
+	}
+	
+	#[inline]
+	fn write_byte(&mut self, register: u8, value: u8) -> Result<(), I2C::Error> {
+		self.i2c.write(Self::ADDRESS, &[register, value])
+	}
+	
+	#[inline]
+	fn read_byte(&mut self, register: u8) -> Result<u8, I2C::Error> {
+		let mut buf = [0x00];
+		self.read_buf(register, &mut buf)?;
+
+		Ok(buf[0])
+	}
 }
